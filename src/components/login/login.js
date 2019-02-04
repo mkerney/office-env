5b35738419207aa5858d6d53fd38d80457e2a78e
mohan-wal <mohan.k@westagilelabs.com>
2019-02-04 21:50:06 +0530

    Added Base Ui for the Project

---
+++ src/components/login/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div className="login-section">
+				MassHealth New MMIS Managed Care Daily Test Data
+			</div>
+        )
+    }
+}
